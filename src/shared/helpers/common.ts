6981fe9d5b9e7edc31dea08f6f1da7c7835bd395
Aleksey <dev.bazylev@gmail.com>
2025-10-19 10:23:33 +0200

    fix

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -1,5 +1,5 @@
 export function generateRandomValue(min:number, max: number, numAfterDigit = 0) {
-  return +((Math.random() * (max - min)) + min).toFixed(numAfterDigit);
+  return parseInt(((Math.random() * (max - min)) + min).toFixed(numAfterDigit), 10);
 }
 
 export function getRandomItems<T>(items: T[], count: number):T[] {
